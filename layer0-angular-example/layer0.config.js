810606c9f9f6a2e47fa6c43bca167d6ccd700038
Tristan Lee <tristanlee85@gmail.com>
2021-03-23 14:35:02 -0400

    Angular rebrand

---
+++ layer0-angular-example/layer0.config.js
@@ -0,0 +1,12 @@
+// This file was automatically added by Layer0 init.
+// You should commit this file to source control.
+module.exports = {
+  connector: '@layer0/angular',
+  routes: './src/layer0/routes.ts',
+  backends: {
+    api: {
+      domainOrIp: 'moovweb-docs-xdn-examples-api-default.moovweb-edge.io',
+      hostHeader: 'moovweb-docs-xdn-examples-api-default.moovweb-edge.io',
+    },
+  },
+}
