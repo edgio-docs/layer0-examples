3168ca5962f48a1105aeb657f8d3e2b5266d620b
howdiz <howard.ross@gmail.com>
2021-04-21 13:32:04 -0400

    add customer error page layout

---
+++ layer0-nuxt-example/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">This is a custom error page</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
